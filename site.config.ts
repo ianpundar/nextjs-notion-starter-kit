3bc6da088600f5a8fa0b8d223ac3aba4c57ace70
Yuang Peng <yuangpeng.cn@gmail.com>
2022-08-12 18:18:31 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '875b064c3f29490fa1a940403e4ab11b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'State of the Alumni',
+  domain: 'https://www.notion.so/yuangpeng/State-of-the-Alumni-875b064c3f29490fa1a940403e4ab11b',
+  author: 'Yuang Peng',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
